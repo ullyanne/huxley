d098002f147bafb92c99d40537c1628ad101b2b4
ullyanne <57293682+ullyanne@users.noreply.github.com>
2019-11-15 01:07:17 -0300

    Create 2809.c
    https://www.thehuxley.com/problem/2809

---
+++ 2809.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int x, algarismo;
+
+	scanf ("%d", &x);
+
+	algarismo = x % 10;
+	printf("Digite um numero:\nAlgarismo das unidades: %d\n", algarismo);
+
+
+
+
+
+	return 0;
+}
