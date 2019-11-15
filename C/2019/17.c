e338b6ce59018cbeee7d9f59d7b47ed1407419fd
ullyanne <57293682+ullyanne@users.noreply.github.com>
2019-11-15 01:15:34 -0300

    Create 17.c
    https://www.thehuxley.com/problem/17/submission/1727351

---
+++ C/2019/17.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int numero, antecessor, sucessor;
+
+
+	scanf("%d", &numero);
+
+
+	antecessor = numero - 1;
+	sucessor = numero + 1;
+	printf("%d %d", antecessor, sucessor);
+
+
+
+
+	return 0;
+}
