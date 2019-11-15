5eeedbc3332d56aef6a6b62cbaa1205fd67abdbb
ullyanne <57293682+ullyanne@users.noreply.github.com>
2019-11-15 15:11:44 -0300

    Create 62 - (1290) L5Q2 - Rickception.c

---
+++ C/2019/62 - (1290) L5Q2 - Rickception.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int ricky_morty (int n, int t, int c)
+{
+	if (c == t)
+	{
+		return n;
+	}
+		
+		else if (c % 2 != 0)
+		{
+			n += (n%5);
+
+			ricky_morty(n, t, c+1);
+		} 
+			else 
+			{
+				n+= 3;
+
+				ricky_morty(n, t, c+1);
+			}
+}
+
+int main()
+{
+	int n, t, resp;
+
+	scanf("%d%d", &n, &t);
+
+	resp = ricky_morty(n, t, 0);
+
+	printf("%d\n", resp);
+}
