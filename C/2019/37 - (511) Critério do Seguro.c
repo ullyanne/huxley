2bd6bd477534216ea21e22413c1f92e3c9b6aab0
ullyanne <57293682+ullyanne@users.noreply.github.com>
2019-11-15 14:46:51 -0300

    Create 37 - (511) Critério do Seguro.c

---
+++ C/2019/37 - (511) Critério do Seguro.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int h, id;
+
+	scanf("%d%d", &h, &id);
+
+	if (h == 0 && id == 1 )
+	{
+		printf("1\n");
+	}
+
+	else
+	{
+		printf("0\n");
+	}
+
+
+	return 0;
+}
